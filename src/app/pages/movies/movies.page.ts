86d3299a7f5fc492fac83f096bb5e34258d813ad
guilhermemm-dev <guilhermemm.dev@gmail.com>
2023-04-02 20:19:49 +0100

    updates

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-movies',
@@ -7,10 +9,29 @@ import { MovieService } from 'src/app/services/movie.service';
   styleUrls: ['./movies.page.scss'],
 })
 export class MoviesPage implements OnInit {
-  constructor(private movieService: MovieService) {}
+  movies = [];
+  currentPage = 1;
+  imageBaseUrl = environment.images;
+
+  constructor(
+    private movieService: MovieService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
-    this.movieService.getTopRatedMovies().subscribe((res) => {
+    this.loadMovies();
+  }
+
+  async loadMovies() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'bubbles',
+    });
+
+    await loading.present();
+
+    this.movieService.getTopRatedMovies(this.currentPage).subscribe((res) => {
+      loading.dismiss();
       console.log(res);
     });
   }
